b0055fca8ad738f18b1cd226a5b3ef92bf9269b6
gitjalm <jonathanalm08@gmail.com>
2025-05-06 13:25:07 +0200

    Massevis av assets og fikset en main menu scene

---
+++ Assets/Scripts/mainMenu.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
