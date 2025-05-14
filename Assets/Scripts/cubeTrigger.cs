19f403e26214143156aa86585ae88b343c093121
gitjalm <jonathanalm08@gmail.com>
2025-05-14 09:53:40 +0200

    ting

--- Assets/Scripts/cubeTrigger.cs
+++ Assets/Scripts/cubeTrigger.cs
@@ -2,8 +2,10 @@ using UnityEngine;
 
 public class cubeTrigger : MonoBehaviour
 {
+    // This method is called when another object enters the trigger
     private void OnTriggerEnter(Collider other)
     {
+        // Check if the object has a specific tag (optional)
         if (other.CompareTag("Player"))
         {
             GetComponent<sceneLoader>().LoadNextScene();
