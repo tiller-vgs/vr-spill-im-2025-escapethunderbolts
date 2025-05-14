e77848511b4a889a56df1fff3a4f21ceeb2d6360
Eliasbk07 <eliasbk2007@icloud.com>
2025-05-14 09:37:25 +0200

    s

--- Assets/Scripts/sceneLoader.cs
+++ Assets/Scripts/sceneLoader.cs
@@ -5,6 +5,7 @@ public class sceneLoader : MonoBehaviour
 {
     public void LoadNextScene()
     {
+        Debug.Log("Loading next scene...");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 }
